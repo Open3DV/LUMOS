8303e194de86588cca12734f5d803132e43256f9
YinyaoCao <caoyinyao@dexforce.com>
2023-07-04 17:52:39 +0800

    修改关键字。

--- version/build.sh
+++ version/build.sh
@@ -4,4 +4,4 @@ commit_time=`date -d@$commit_ts +"%Y-%m-%d %H:%M:%S"`
 current_time=`date +"%Y-%m-%d %H:%M:%S"`
 git_version=`git log -1 --format="%h"`
 camera_model=''
-sed s/MYVERSION/"XEMA$camera_model\ version: $git_version\ commit: $commit_time\ build: $current_time"/g version.h.tmp > ../firmware/version.h
+sed s/MYVERSION/"LUMOS$camera_model\ version: $git_version\ commit: $commit_time\ build: $current_time"/g version.h.tmp > ../firmware/version.h
